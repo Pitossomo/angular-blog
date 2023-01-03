61d8acf1406ecfa17d3be3392622dec05f8fe92f
pitossomo <pedro.h.teles@hotmail.com>
2023-01-03 16:55:03 -0300

    create highlighted post component

---
+++ src/app/components/highlighted-post/highlighted-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlighted-post',
+  templateUrl: './highlighted-post.component.html',
+  styleUrls: ['./highlighted-post.component.css']
+})
+export class HighlightedPostComponent {
+
+}
