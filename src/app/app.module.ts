084c5c9dc55f78c2dffb7328f16e093a01e32a72
pitossomo <pedro.h.teles@hotmail.com>
2023-01-03 16:56:03 -0300

    add new components to app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { TitleTextsComponent } from './components/title-texts/title-texts.component';
+import { HighlightedPostComponent } from './components/highlighted-post/highlighted-post.component';
+import { SmallPostComponent } from './components/small-post/small-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    TitleTextsComponent,
+    HighlightedPostComponent,
+    SmallPostComponent
   ],
   imports: [
     BrowserModule
