f0906195c03afd35e287cc3aef04186a1b932012
pitossomo <pedro.h.teles@hotmail.com>
2023-01-03 16:55:20 -0300

    create title texts component

---
+++ src/app/components/title-texts/title-texts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-texts',
+  templateUrl: './title-texts.component.html',
+  styleUrls: ['./title-texts.component.css']
+})
+export class TitleTextsComponent {
+
+}
