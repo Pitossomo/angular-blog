9b1dad28b26ddf2817449bd4841e21a56571ec88
pitossomo <pedro.h.teles@hotmail.com>
2023-01-05 08:08:21 -0300

    new shared component registered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { TitleTextsComponent } from './components/title-texts/title-texts.component';
-import { HighlightedPostComponent } from './components/highlighted-post/highlighted-post.component';
+import { BigPostComponent } from './components/big-post/big-post.component';
 import { SmallPostComponent } from './components/small-post/small-post.component';
+import { PostInfoComponent } from './shared/post-info/post-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderBarComponent,
     TitleTextsComponent,
-    HighlightedPostComponent,
-    SmallPostComponent
+    BigPostComponent,
+    SmallPostComponent,
+    PostInfoComponent
   ],
   imports: [
     BrowserModule
