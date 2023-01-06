0fe149395e6a61ce0f8f75ad4779cb84cc6e8560
pitossomo <pedro.h.teles@hotmail.com>
2023-01-06 06:56:12 -0300

    set font-size dynamically for post-info component

--- src/app/shared/post-info/post-info.component.ts
+++ src/app/shared/post-info/post-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-info',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-info.component.css']
 })
 export class PostInfoComponent {
-
+  @Input() size = 'big'
 }
