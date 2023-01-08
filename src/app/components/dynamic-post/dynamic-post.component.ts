6ded8b056f228831a5bcb7af9f1f17de23b2d895
pitossomo <pedro.h.teles@hotmail.com>
2023-01-08 20:47:48 -0300

    make posts dynamic wth post info within it

--- src/app/components/dynamic-post/dynamic-post.component.ts
+++ src/app/components/dynamic-post/dynamic-post.component.ts
@@ -1,10 +1,36 @@
 import { Component, Input } from '@angular/core';
 
+export interface IPost {
+  id: string,
+  title: string,
+  imgAlt: string,
+  category: string,
+  durationInMinutes: number,
+  author: {
+    name: string,
+    id: string
+  },
+  date: string
+}
+
 @Component({
   selector: 'app-dynamic-post',
   templateUrl: './dynamic-post.component.html',
   styleUrls: ['./dynamic-post.component.css']
 })
+
 export class DynamicPostComponent {
-  @Input() size = 'small'
+  @Input() size: string = 'small'
+  @Input() post: IPost = {
+    id: '1',
+    title: 'The 3 Best Practices in Sales Prospecting',
+    imgAlt: 'placeholder',
+    category: 'Sales Ops',
+    durationInMinutes: 8,
+    author: {
+      name: 'Pitossomo',
+      id: '1'
+    },
+    date: '07 jan 2023'
+  }
 }
